3c4cfa0f163c2d42348128c041d4ef62d0a0d53d
Andrew Stucki <andrew.stucki@redpanda.com>
2024-08-01 17:21:37 -0400

    Add basic user and acl definitions

--- src/go/k8s/api/redpanda/v1alpha2/redpanda_types.go
+++ src/go/k8s/api/redpanda/v1alpha2/redpanda_types.go
@@ -292,3 +292,147 @@ func (in *Redpanda) GetMigrationClusterNamespace() string {
 	}
 	return namespace
 }
+
+// ClusterRef represents a reference to a cluster that is being targeted.
+type ClusterRef struct {
+	// Namespace specifies namespace of the cluster being referenced. If empty, the namespace
+	// of the referencing object will be used.
+	Namespace string `json:"namespace,omitempty"`
+	// Name specifies the name of the cluster being referenced.
+	Name string `json:"name,omitempty"`
+}
+
+// Redpanda defines the CRD for Redpanda user.
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=redpandaUsers
+// +kubebuilder:resource:shortName=rpu
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].message",description=""
+// +kubebuilder:storageversion
+type RedpandaUser struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Defines the desired state of the Redpanda user.
+	Spec RedpandaUserSpec `json:"spec,omitempty"`
+	// Represents the current status of the Redpanda user.
+	Status RedpandaUserStatus `json:"status,omitempty"`
+}
+
+// RedpandaUserStatus defines the observed state of a Redpanda user
+type RedpandaUserStatus struct {
+	// Specifies the last observed generation.
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+
+	// Conditions holds the conditions for the Redpanda user.
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// RedpandaUserSpec defines a user of a Redpanda cluster.
+type RedpandaUserSpec struct {
+	// ClusterRef is a reference to the cluster where the user should be created.
+	ClusterRef ClusterRef `json:"clusterRef,omitempty"`
+	// Authentication defines the authentication information for a user.
+	// If authentication is not configured, no credentials are generated.
+	// +optional
+	Authentication RedpandaUserAuthenticationSpec `json:"authentication,omitempty`
+	// Authorization rules defined for this user.
+	// +optional
+	Authorization RedpandaUserAuthorizationSpec `json:"authorization,omitempty"`
+	// Quotas on requests to control the broker resources used by clients. Network
+	// bandwidth and request rate quotas can be enforced.
+	// +optional
+	Quotas RedpandaQuotasSpec `json:"quotas,omitempty"`
+	// Template to specify how user secrets are generated.
+	// +optional
+	Template RedpandaUserTemplateSpec `json:"template,omitempty"`
+}
+
+// RedpandaUserTemplateSpec defines the template metadata for a user
+type RedpandaUserTemplateSpec struct {
+	// Template for RedpandaUser resources. The template allows users
+	// to specify how the Secret with password or TLS certificates is generated. 
+	Secret ResourceTemplate `json:"secret,omitempty"`
+}
+
+// ResourceTemplate specifies additional configuration for a resource.
+type ResourceTemplate struct {
+	// Metadata specifies additional metadata to associate with a resource.
+	Metadata MetadataTemplate `json:"metadata,omitempty"`
+}
+
+// MetadataTemplate defines additional metadata to associate with a resource.
+type MetadataTemplate struct {
+	// Labels specifies the Kubernetes labels to apply to a managed resource.
+	// +optional
+	Labels map[string]string `json:"labels,omitempty"`
+	// Annotations specifies the Kubernetes annotations to apply to a managed resource.
+	// +optional
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+// RedpandaQuotasSpec defines the quotas for a user.
+type RedpandaQuotasSpec struct {
+	// A quota on the maximum bytes per-second that each client group can publish to a broker
+	// before the clients in the group are throttled. Defined on a per-broker basis.
+	// +optional
+	ProducerByteRate *int `json:"producerByteRate,omitempty"`
+	// A quota on the maximum bytes per-second that each client group can fetch from a broker
+	// before the clients in the group are throttled. Defined on a per-broker basis.
+	// +optional
+	ConsumerByteRate *int `json:"consumerByteRate,omitempty"`
+	// A quota on the maximum CPU utilization of each client group as a percentage of network and I/O threads.
+	// +optional
+	RequestPercentage *int `json:"requestPercentage,omitempty"`
+	// A quota on the rate at which mutations are accepted for the create topics request, the create
+	// partitions request and the delete topics request. The rate is accumulated by the number of partitions
+	// created or deleted.
+	// +optional
+	ControllerMutationRate *int `json:"controllerMutationRate,omitempty"`
+}
+
+// RedpandaUserAuthenticationSpec defines the authentication mechanism enabled for this Redpanda user.
+type RedpandaUserAuthenticationSpec struct {
+	// one of: scram-sha-512, tls, or tls-external
+	Type string `json:"type,omitempty"`
+}
+
+// Authorization rules for this user.
+type RedpandaUserAuthorizationSpec struct {
+	// one of: simple
+	Type string `json:"type,omitempty"`
+	// List of ACL rules which should be applied to this user.
+	ACLs []RedpandaACLRule `json:"acls,omitempty"`
+}
+
+// Defines an ACL rule applied to the given user.
+type RedpandaACLRule struct {
+	// one of: allow, deny
+	Type string `json:"type,omitempty"`
+	// Indicates the resource for which given ACL rule applies.
+	Resource RedpandaACLResourceSpec `json:"resource,omitempty"`
+	// The host from which the action described in the ACL rule is allowed or denied.
+	// If not set, it defaults to *, allowing or denying the action from any host.
+	Host string `json:"host,omitempty"`
+	// List of operations which will be allowed or denied.
+	// one or more of [Read, Write, Delete, Alter, Describe, All, IdempotentWrite, ClusterAction, Create, AlterConfigs, DescribeConfigs]
+	Operations []string `json:"operations,omitempty"`
+}
+
+// Indicates the resource for which given ACL rule applies.
+type RedpandaACLResourceSpec struct {
+	// one of: topic, group, cluster, transactionalId
+	Type string `json:"type,omitempty"`
+	// Name of resource for which given ACL rule applies.
+	// Can be combined with patternType field to use prefix pattern.
+	Name string `json:"name,omitempty"`
+	// Describes the pattern used in the resource field. The supported types are literal
+	// and prefix. With literal pattern type, the resource field will be used as a definition
+	// of a full topic name. With prefix pattern type, the resource name will be used only as
+	// a prefix. Default value is literal.
+	// 
+	// one of: prefix, literal
+	PatternType string `json:"patternType,omitempty"`
+}
