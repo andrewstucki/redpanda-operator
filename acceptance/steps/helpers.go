248df96d85692cd1315c9a1baa66f5d7b2c6e0ac
Andrew Stucki <andrew.stucki@redpanda.com>
2024-09-10 12:51:31 -0400

    lightly refactor steps

---
+++ acceptance/steps/helpers.go
@@ -0,0 +1,177 @@
+package steps
+
+import (
+	"context"
+	"encoding/json"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/cucumber/godog"
+	"github.com/redpanda-data/common-go/rpadmin"
+	"github.com/redpanda-data/helm-charts/pkg/kube"
+	framework "github.com/redpanda-data/redpanda-operator/harpoon"
+	redpandav1alpha2 "github.com/redpanda-data/redpanda-operator/src/go/k8s/api/redpanda/v1alpha2"
+	"github.com/redpanda-data/redpanda-operator/src/go/k8s/pkg/client"
+	"github.com/redpanda-data/redpanda-operator/src/go/k8s/pkg/client/acls"
+	"github.com/redpanda-data/redpanda-operator/src/go/k8s/pkg/client/users"
+	"github.com/stretchr/testify/require"
+	"github.com/twmb/franz-go/pkg/kgo"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type clusterClients struct {
+	Kafka         *kgo.Client
+	RedpandaAdmin *rpadmin.AdminAPI
+	Users         *users.Client
+	ACLs          *acls.Syncer
+
+	cluster string
+	t       framework.TestingT
+}
+
+func (c *clusterClients) ExpectUser(ctx context.Context, user string) {
+	c.t.Logf("Checking for user %q in cluster %q", user, c.cluster)
+	c.checkUser(ctx, user, true)
+	c.t.Logf("Found user %q in cluster %q", user, c.cluster)
+}
+
+func (c *clusterClients) ExpectNoUser(ctx context.Context, user string) {
+	c.t.Logf("Checking that user %q does not exist in cluster %q", user, c.cluster)
+	c.checkUser(ctx, user, false)
+	c.t.Logf("Found no user %q in cluster %q", user, c.cluster)
+}
+
+func (c *clusterClients) checkUser(ctx context.Context, user string, exists bool) {
+	require.Eventually(c.t, func() bool {
+		users, err := c.RedpandaAdmin.ListUsers(ctx)
+		require.NoError(c.t, err)
+
+		return exists == slices.Contains(users, user)
+	}, 10*time.Second, 1*time.Second)
+}
+
+func clientsForCluster(t framework.TestingT, ctx context.Context, cluster string, user *redpandav1alpha2.User) *clusterClients {
+	// we construct a fake user to grab all of the clients for the cluster
+	referencer := &redpandav1alpha2.User{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: t.Namespace(),
+		},
+		Spec: redpandav1alpha2.UserSpec{
+			ClusterSource: &redpandav1alpha2.ClusterSource{
+				ClusterRef: &redpandav1alpha2.ClusterRef{
+					Name: cluster,
+				},
+			},
+		},
+	}
+
+	factory := client.NewFactory(t.RestConfig(), t).WithDialer(kube.NewPodDialer(t.RestConfig()).DialContext)
+
+	if user != nil && user.Spec.Authentication != nil {
+		username := user.Name
+		password, err := user.Spec.Authentication.Password.Fetch(ctx, t, user.Namespace)
+		require.NoError(t, err)
+		mechanism := string(*user.Spec.Authentication.Type)
+
+		factory = factory.WithUserAuth(&client.UserAuth{
+			Username:  username,
+			Password:  password,
+			Mechanism: mechanism,
+		})
+	}
+
+	kafka, err := factory.KafkaClient(ctx, referencer)
+	require.NoError(t, err)
+
+	redpanda, err := factory.RedpandaAdminClient(ctx, referencer)
+	require.NoError(t, err)
+
+	users, err := factory.Users(ctx, referencer)
+	require.NoError(t, err)
+
+	syncer, err := factory.ACLs(ctx, referencer)
+	require.NoError(t, err)
+
+	return &clusterClients{
+		Kafka:         kafka,
+		RedpandaAdmin: redpanda,
+		Users:         users,
+		ACLs:          syncer,
+		cluster:       cluster,
+		t:             t,
+	}
+}
+
+func usersFromTable(t framework.TestingT, cluster string, table *godog.Table) []*redpandav1alpha2.User {
+	var users []*redpandav1alpha2.User
+
+	for i, row := range table.Rows {
+		// skip the header row:
+		// | name | password | mechanism | acls |
+		if i == 0 {
+			continue
+		}
+		name, password, mechanism, acls := row.Cells[0].Value, row.Cells[1].Value, row.Cells[2].Value, row.Cells[3].Value
+		name, password, mechanism, acls = strings.TrimSpace(name), strings.TrimSpace(password), strings.TrimSpace(mechanism), strings.TrimSpace(acls)
+		user := &redpandav1alpha2.User{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: t.Namespace(),
+				Name:      name,
+			},
+			Spec: redpandav1alpha2.UserSpec{
+				ClusterSource: &redpandav1alpha2.ClusterSource{
+					ClusterRef: &redpandav1alpha2.ClusterRef{
+						Name: cluster,
+					},
+				},
+			},
+		}
+		if mechanism != "" || password != "" {
+			user.Spec.Authentication = &redpandav1alpha2.UserAuthenticationSpec{
+				Type: ptr.To(redpandav1alpha2.SASLMechanism(mechanism)),
+				Password: redpandav1alpha2.Password{
+					Value: password,
+					ValueFrom: &redpandav1alpha2.PasswordSource{
+						SecretKeyRef: &corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: name + "-password",
+							},
+						},
+					},
+				},
+			}
+		}
+		if acls != "" {
+			user.Spec.Authorization = &redpandav1alpha2.UserAuthorizationSpec{}
+			require.NoError(t, json.Unmarshal([]byte(acls), &user.Spec.Authorization.ACLs))
+		}
+
+		users = append(users, user)
+	}
+
+	return users
+}
+
+func checkStableResource(ctx context.Context, t framework.TestingT, o runtimeclient.Object) {
+	var previousResourceVersion string
+	var equalityChecks int
+
+	require.Eventually(t, func() bool {
+		require.NoError(t, t.Get(ctx, runtimeclient.ObjectKeyFromObject(o), o))
+		if previousResourceVersion == o.GetResourceVersion() {
+			equalityChecks++
+		} else {
+			equalityChecks = 0
+		}
+		// ensure we're stable for a 5 second window
+		if equalityChecks == 5 {
+			return true
+		}
+		previousResourceVersion = o.GetResourceVersion()
+		return false
+	}, 30*time.Second, 1*time.Second)
+}
--- acceptance/steps/users.go
+++ acceptance/steps/users.go
@@ -2,132 +2,13 @@ package steps
 
 import (
 	"context"
-	"encoding/json"
-	"slices"
-	"strings"
-	"time"
 
 	"github.com/cucumber/godog"
-	"github.com/redpanda-data/common-go/rpadmin"
-	"github.com/redpanda-data/helm-charts/pkg/kube"
 	framework "github.com/redpanda-data/redpanda-operator/harpoon"
 	redpandav1alpha2 "github.com/redpanda-data/redpanda-operator/src/go/k8s/api/redpanda/v1alpha2"
-	"github.com/redpanda-data/redpanda-operator/src/go/k8s/pkg/client"
-	"github.com/redpanda-data/redpanda-operator/src/go/k8s/pkg/client/acls"
-	"github.com/redpanda-data/redpanda-operator/src/go/k8s/pkg/client/users"
 	"github.com/stretchr/testify/require"
-	"github.com/twmb/franz-go/pkg/kgo"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/utils/ptr"
 )
-
-type clusterClients struct {
-	Kafka         *kgo.Client
-	RedpandaAdmin *rpadmin.AdminAPI
-	Users         *users.Client
-	ACLs          *acls.Syncer
-}
-
-func clientsForCluster(t framework.TestingT, ctx context.Context, cluster string, user *redpandav1alpha2.User) *clusterClients {
-	// we construct a fake user to grab all of the clients for the cluster
-	referencer := &redpandav1alpha2.User{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: t.Namespace(),
-		},
-		Spec: redpandav1alpha2.UserSpec{
-			ClusterSource: &redpandav1alpha2.ClusterSource{
-				ClusterRef: &redpandav1alpha2.ClusterRef{
-					Name: cluster,
-				},
-			},
-		},
-	}
-
-	factory := client.NewFactory(t.RestConfig(), t).WithDialer(kube.NewPodDialer(t.RestConfig()).DialContext)
-
-	if user != nil && user.Spec.Authentication != nil {
-		username := user.Name
-		password, err := user.Spec.Authentication.Password.Fetch(ctx, t, user.Namespace)
-		require.NoError(t, err)
-		mechanism := string(*user.Spec.Authentication.Type)
-
-		factory = factory.WithUserAuth(&client.UserAuth{
-			Username:  username,
-			Password:  password,
-			Mechanism: mechanism,
-		})
-	}
-
-	kafka, err := factory.KafkaClient(ctx, referencer)
-	require.NoError(t, err)
-
-	redpanda, err := factory.RedpandaAdminClient(ctx, referencer)
-	require.NoError(t, err)
-
-	users, err := factory.Users(ctx, referencer)
-	require.NoError(t, err)
-
-	syncer, err := factory.ACLs(ctx, referencer)
-	require.NoError(t, err)
-
-	return &clusterClients{
-		Kafka:         kafka,
-		RedpandaAdmin: redpanda,
-		Users:         users,
-		ACLs:          syncer,
-	}
-}
-
-func usersFromTable(t framework.TestingT, cluster string, table *godog.Table) []*redpandav1alpha2.User {
-	var users []*redpandav1alpha2.User
-
-	for i, row := range table.Rows {
-		// skip the header row:
-		// | name | password | mechanism | acls |
-		if i == 0 {
-			continue
-		}
-		name, password, mechanism, acls := row.Cells[0].Value, row.Cells[1].Value, row.Cells[2].Value, row.Cells[3].Value
-		name, password, mechanism, acls = strings.TrimSpace(name), strings.TrimSpace(password), strings.TrimSpace(mechanism), strings.TrimSpace(acls)
-		user := &redpandav1alpha2.User{
-			ObjectMeta: metav1.ObjectMeta{
-				Namespace: t.Namespace(),
-				Name:      name,
-			},
-			Spec: redpandav1alpha2.UserSpec{
-				ClusterSource: &redpandav1alpha2.ClusterSource{
-					ClusterRef: &redpandav1alpha2.ClusterRef{
-						Name: cluster,
-					},
-				},
-			},
-		}
-		if mechanism != "" || password != "" {
-			user.Spec.Authentication = &redpandav1alpha2.UserAuthenticationSpec{
-				Type: ptr.To(redpandav1alpha2.SASLMechanism(mechanism)),
-				Password: redpandav1alpha2.Password{
-					Value: password,
-					ValueFrom: &redpandav1alpha2.PasswordSource{
-						SecretKeyRef: &corev1.SecretKeySelector{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: name + "-password",
-							},
-						},
-					},
-				},
-			}
-		}
-		if acls != "" {
-			user.Spec.Authorization = &redpandav1alpha2.UserAuthorizationSpec{}
-			require.NoError(t, json.Unmarshal([]byte(acls), &user.Spec.Authorization.ACLs))
-		}
-
-		users = append(users, user)
-	}
-
-	return users
-}
 
 func iCreateCRDbasedUsers(ctx context.Context, cluster string, users *godog.Table) error {
 	t := framework.T(ctx)
@@ -135,6 +16,16 @@ func iCreateCRDbasedUsers(ctx context.Context, cluster string, users *godog.Tabl
 	for _, user := range usersFromTable(t, cluster, users) {
 		t.Logf("Creating user %q", user.Name)
 		require.NoError(t, t.Create(ctx, user))
+
+		// make sure the resource is stable
+		checkStableResource(ctx, t, user)
+
+		// make sure it's synchronized
+		t.RequireCondition(metav1.Condition{
+			Type:   redpandav1alpha2.UserConditionTypeSynced,
+			Status: metav1.ConditionTrue,
+			Reason: redpandav1alpha2.UserConditionReasonSynced,
+		}, user.Status.Conditions)
 	}
 
 	return nil
@@ -143,27 +34,14 @@ func iCreateCRDbasedUsers(ctx context.Context, cluster string, users *godog.Tabl
 func shouldExistAndBeAbleToAuthenticateToTheCluster(ctx context.Context, user, cluster string) error {
 	t := framework.T(ctx)
 
-	clients := clientsForCluster(t, ctx, cluster, nil)
-
-	t.Logf("Checking for user %q in cluster %q", user, cluster)
-	require.Eventually(t, func() bool {
-		users, err := clients.Users.List(ctx)
-		require.NoError(t, err)
-
-		return slices.Contains(users, user)
-	}, 10*time.Second, 1*time.Second)
-	t.Logf("Found user %q in cluster %q", user, cluster)
+	clientsForCluster(t, ctx, cluster, nil).ExpectUser(ctx, user)
 
 	// Now we do the same check, but authenticated as the user
 
 	var userObject redpandav1alpha2.User
 	require.NoError(t, t.Get(ctx, t.ResourceKey(user), &userObject))
 
-	clients = clientsForCluster(t, ctx, cluster, &userObject)
-	users, err := clients.RedpandaAdmin.ListUsers(ctx)
-
-	require.NoError(t, err)
-	require.Contains(t, users, user)
+	clientsForCluster(t, ctx, cluster, &userObject).ExpectUser(ctx, user)
 
 	return nil
 }
@@ -171,16 +49,7 @@ func shouldExistAndBeAbleToAuthenticateToTheCluster(ctx context.Context, user, c
 func thereIsNoUser(ctx context.Context, user, cluster string) error {
 	t := framework.T(ctx)
 
-	clients := clientsForCluster(t, ctx, cluster, nil)
-
-	t.Logf("Checking that user %q does not exist in cluster %q", user, cluster)
-	require.Eventually(t, func() bool {
-		users, err := clients.Users.List(ctx)
-		require.NoError(t, err)
-
-		return !slices.Contains(users, user)
-	}, 10*time.Second, 1*time.Second)
-	t.Logf("Found no user %q in cluster %q", user, cluster)
+	clientsForCluster(t, ctx, cluster, nil).ExpectNoUser(ctx, user)
 
 	return nil
 }
